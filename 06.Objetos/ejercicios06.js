e6d5ef9af5d12aef175daef0e8514db76cd0dcdb
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2023-10-31 17:30:07 -0300

    Finish ejercicio06 8test

--- 06.Objetos/ejercicios06.js
+++ 06.Objetos/ejercicios06.js
@@ -2,45 +2,52 @@
 // Completa el siguiente objeto 'alumnos' que tenga como primera propiedad nombre, segunda propiedad apellido, tercera edad y cuarta examen
 // nombre y apellido deben tener como valor un string, edad un entero y examen un booleano.
 // Tu código:
-let alumnos = {}
+let alumnos = {
+    nombre: "Lautaro",
+    apellido: "Gonzalez",
+    edad: 24,
+    examen: true
+}
 
 
-function datosObj(obj){
+function datosObj(obj) {
     // Pasado el objeto por paramentros compuesto de la siguiente manera : 
     //{ nombre: Leonardo, apellido: Avila }
     // obtener los valeres nombre y apellido y dar un mensaje de bienvenida 
     // 'Bienvenido Leonardo Avila' 
     // tu código aqui:
-
+    return `Bienvenido ${obj.nombre} ${obj.apellido}`;
 }
 
-function cambiandoPropiedades(obj){
+function cambiandoPropiedades(obj) {
     // Pasado el objeto por paramentros compuesto de la siguiente manera : 
     //{ nombre: Pablo, apellido: Perez, edad: 25 }
     // Agregar una propiedad 'nacionalidad' con el valor de 'Argentina'
     // tu código aqui:
-
+    obj.nacionalidad = 'Argentina';
+    return obj;
 }
 
 function eliminarPropiedad(objeto, propiedad) {
     // Esta funcion recibe por parametros un objeto y una propiedad del mismo objeto.
     // tendrás que eliminar esa propiedad del objeto y devolver el objeto
     // Tu código:
-
+    delete objeto[propiedad];
+    return objeto;
 }
 
 function contieneEmail(user) {
     // Devuelve true si el user contiene la propiedad de 'email', si no contiene devuelve false
     // Tu código:
-
+    return user.email ? true : false;
 }
 
 function verificarPassword(user) {
     // Por parametros se pasa una objeto el cual tiene una propriedad 'password'.
     // comprueba si el valor de esta propiedad tiene un longitud mayor o igual a 8 caracteres
-    // si cumple devolver true, caso contrario devolver 'Por favor, pon una contraseña más segura'
+    // si cumple devolver true, caso contrario devolver 'Porfavor, pon una contraseña más segura'
     // Tu código:
-
+    return user.password.length >= 8 ? true : 'Porfavor, pon una contraseña más segura';
 }
 
 function permisosDeEntrada(personas) {
@@ -49,20 +56,28 @@ function permisosDeEntrada(personas) {
     // si contiene entrada (true) y si es mayor de edad. agregar en un nuevo array, las personas que cumplan
     // el nuevo array debe contener unicamente el nombre de las personas
     // tu código:
-
+    personasHabilitadas = [];
+    personas.forEach(persona => {
+        persona.entrada && (persona.edad >=18) ? personasHabilitadas.push(persona.nombre):null;
+    });
+    return personasHabilitadas;
 }
 
 //Crea un objeto "libro" con propiedades como título, autor y año de publicación. 
 //Luego, completa la función que toma ese objeto como argumento
-//devuelva la información del libro de la siguiente manera: "El libro {título}, escrito por {autor} , fue publicado en {añoPublicación}".
+//devuelva la información del libro de la siguiente manera: "El libro {título}, escrito por {autor} , fue publicado en {añoPublicación}."
 //tu codigo:
 
-let libro = {};
-  
-  // Función para mostrar la información del libro
-  function mostrarInformacionLibro(libro) {
+let libro = {
+    título: "20000 leguas de viaje submarino",
+    autor: "Julio Verne",
+    añoPublicación: 1870
+};
 
-  }
+// Función para mostrar la información del libro
+function mostrarInformacionLibro(libro) {
+    return `El libro ${libro.título}, escrito por ${libro.autor} , fue publicado en ${libro.añoPublicación}.`;
+}
 
 module.exports = {
     alumnos,
@@ -74,4 +89,4 @@ module.exports = {
     permisosDeEntrada,
     libro,
     mostrarInformacionLibro
-  };
+};
