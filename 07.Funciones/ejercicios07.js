bb05593fcfb727af5beaa422b5a75addbbca9a98
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2023-11-02 18:03:29 -0300

    Update Ejercicios 7

--- 07.Funciones/ejercicios07.js
+++ 07.Funciones/ejercicios07.js
@@ -4,17 +4,24 @@
 //Debes crear un funcion que retorne un mensaje de bienvenida con nombre y apellido de la persona
 //Ejemplo "Bienvenido Leonardo Avila" -> IMPORTANTE la funcion tiene que llamarse 'bienvenida'
 //Tu código:
-
-
-
+function bienvenida(name, surname) {
+    return `Bienvenido ${name} ${surname}`;
+}
 /**********************************************************************************/
 // Crea una funcion el cual cumpla la funcion de una calculadora.. tiene que recibir 3 parametros num1, operador y num2
 // IMPORTANTE -> la funcion debe llamarse 'calc'
 // Tu código:
-
-
-
-
+function calc(num1, operador, num2) {
+    if (operador === "+") {
+        return num1 + num2;
+    } else if (operador === "-") {
+        return num1 - num2;
+    } else if (operador === "*") {
+        return num1 * num2;
+    } else if (operador === "/") {
+        return num1 / num2;
+    }
+}
 /**********************************************************************************/
 // Crea una funcion que en base a la temperaturma maxima, min y porcentaje de lluvia, diga si es necesario salir con abrigo,
 // paraguas o sin nada. ejemplos
@@ -25,41 +32,85 @@
 // ejemplo => max = 20 y porcentaje de lluvia = 70 => devolver "El día está lindo, recomendable un paraguas"
 // IMPORTANTE -> la funcion debe llamarse 'temp'    
 // Tu código:
-
-
-
-
+function temp(max,min,percentLluvia){
+    let msj
+    if(max<15){
+        msj = 'Es recomendable salir con abrigo';
+    }else if(max<26 && min >17){
+        msj = 'El día está lindo';
+    }else if(min>25){
+        msj = 'El día estará caluroso';
+    }
+    
+    return percentLluvia > 50 ? `${msj}, recomendable un paraguas`:msj;
+}
 /**********************************************************************************/
 
 // Crea una función que reciba un array de strings como parámetro y devuelva un nuevo array con solo los strings que tienen una longitud mayor a 5.
 // IMPORTANTE -> la funcion debe llamarse 'arrayDeStrings'
 // Tu código:
-
-
-
-
+function arrayDeStrings(arr){
+    let arrFilter
+    arr.forEach(word => {
+        if(arr.length > 5) arrFilter.push(word)
+    });
+    return arrFilter;
+}
 /**********************************************************************************/
 // Crea una función que reciba un string como parámetro y devuelva el mismo string pero con todas las palabras en orden inverso.
 // IMPORTANTE -> la funcion debe llamarse 'stringInverso'
 // Tu código:
-
-
-
+function stringInverso(word){
+    return word.split('').reverse().join('')
+}
 /**********************************************************************************/
 // Crea una función que reciba un array de objetos con propiedades "nombre" y "edad" y devuelva el nombre de la persona más joven.
 // IMPORTANTE -> la funcion debe llamarse 'personaMásJoven'
 // Tu código:
-
-
-
+function personaMásJoven(personas){
+    let menor;
+    personas.forEach(persona => {
+        if(persona.edad < menor){
+            menor = persona
+        }
+    });
+    return menor;
+}
 /**********************************************************************************/
 // Crea una función que reciba una cadena de texto y devuelva un objeto con la cantidad de veces que aparece cada letra en la cadena.
 // IMPORTANTE -> la funcion debe llamarse 'contadorDeLetras'
 // Tu código:
-
-
-
-
+function contadorDeLetras(palabra){
+    cantLetras = {
+        a:0,
+        e:0,
+        i:0,
+        o:0,
+        u:0
+    }
+    palabra.forEach(letra => {
+        palabra = palabra.split('');
+        switch (letra) {
+            case a:
+                cantLetras.a +=1
+                break;
+            case e:
+                cantLetras.e +=1
+                break;
+            case i:
+                cantLetras.i +=1
+                break;
+            case o:
+                cantLetras.o +=1
+                break;
+            case u:
+                cantLetras.u +=1
+                break;
+            default:
+                break;
+        }
+    });
+}
 /**********************************************************************************/
 // Crea una función que reciba un array de objetos que tiene como propiedades "nombre" y "sueldo" y devuelva el objeto con el sueldo más alto.
 // IMPORTANTE -> la funcion debe llamarse 'sueldoMasAlto'
